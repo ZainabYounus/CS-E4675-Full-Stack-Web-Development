df56cdf472c3956f0246b39c7ce75214148c0a3f
ZainabYounus <zainabyounus63@gmail.com>
2022-11-28 18:26:14 +0200

    1.2: course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,49 +1,63 @@
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
-    // <div>
-    //   <h1>{props.course}</h1>
-    // </div>
   )
 }
 
-
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.partName} {props.exerciseNumber}
     </p>
   )
 }
+const Content = (props) => {
+  return (
+    <div>
+      <Part  partName = {props.parts[0].name} exerciseNumber = {props.parts[0].exercises}/>
+      <Part  partName = {props.parts[1].name} exerciseNumber = {props.parts[1].exercises}/>
+      <Part  partName = {props.parts[2].name} exerciseNumber = {props.parts[2].exercises}/>
+    </div>
+  )
+}
+
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.exercises}</p>
   )
 }
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const partsInfo = [
+    {
+      name : 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name : 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name : 'State of a component',
+      exercises: 14
+    }
+  ]
 
+  let totalExercises = partsInfo[0].exercises + partsInfo[1].exercises + partsInfo[2].exercises 
+  console.log(totalExercises)
   return (
     <div>
       {/* Calling Header component to render heading */}
       <Header course = {course} />
 
       {/* Calling Content component to render content */}
-      <Content partName = {part1} exerciseNumber = {exercises1}/>
-      <Content partName = {part2} exerciseNumber = {exercises2}/>
-      <Content partName = {part3} exerciseNumber = {exercises3}/>
+      <Content parts = {partsInfo}/>
 
       {/* Calling Total component to render total of exercises */}
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
+      <Total exercises = {totalExercises}/>
     </div>
   )
 }
