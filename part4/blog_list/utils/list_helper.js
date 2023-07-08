5a6300792c079ce5920e822bd3fa81b2f3ee8ee2
ZainabYounus <zainabyounus63@gmail.com>
2023-07-09 02:12:27 +0300

    Modified 4.7

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -42,11 +42,29 @@ const mostBlogs = (blogs) => {
 	return blogs.length === 0 ? {} : { author: blog, blogs: blogsCountByAuthor[blog] }
 }
 
+
+// const mostLikes = (blogs) => {
+// 	const sortedByLikes = lodash.sortBy(blogs, 'likes')
+// 	const blogWithMostLikes = sortedByLikes[sortedByLikes.length - 1]
+// 	return blogWithMostLikes.length === 0 ? {} : { 'author' : blogWithMostLikes.author, 'likes': blogWithMostLikes.likes }
+// }
+
 const mostLikes = (blogs) => {
-	const sortedByLikes = lodash.sortBy(blogs, 'likes')
-	const blogWithMostLikes = sortedByLikes[sortedByLikes.length - 1]
-	return blogWithMostLikes.length === 0 ? {} : { 'author' : blogWithMostLikes.author, 'likes': blogWithMostLikes.likes }
-}
+	const authors = lodash.uniq(blogs.map(blog => blog.author))
+  
+	authorLikes = []
+	lodash.forEach(authors, (author) => {
+	  let sum = lodash.sumBy(blogs, (o) => { 
+		return o.author === author ? o.likes : 0
+	  })
+	  authorLikes.push({ author: author, likes: sum })
+	})
+	const mostLikes = Math.max(...authorLikes.map(author => author.likes))
+	const authorWithMostLikes = authorLikes.find(author => author.likes === mostLikes)
+	
+	return blogs.length === 0 ? {} : authorWithMostLikes
+  
+  }
 
 module.exports = {
 	dummy,
